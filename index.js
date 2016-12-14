872384139c307a4bb8fa5206347ec176843e334c
Tim Douglas <neurotech@gmail.com>
2016-12-14 22:17:36 +1100

    Added simple GPIO support

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+const raspi = require('raspi-io');
+const five = require('johnny-five');
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: process.env.PI_LIGHT_HTTP_PORT });
-
-server.route({
-  method: 'POST',
-  path: '/light',
-  handler: function (request, reply) {
-    console.log('Light called!');
-    reply('Hello!');
-  }
+const board = new five.Board({
+  io: new raspi(),
+  repl: false
 });
 
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at: ${server.info.uri}`);
+let pin = 'P1-12';
+let step = 2000;
+
+server.connection({ port: process.env.PI_LIGHT_HTTP_PORT });
+
+board.on('ready', function () {
+
+});
+
+board.on('ready', function () {
+  let torch = new five.Led(pin);
+
+  server.route({
+    method: 'POST',
+    path: '/light/{command}',
+    handler: function (request, reply) {
+      if (request.params.command === 'on') {
+	torch.pulse(step);
+        reply('Light ON');
+      }
+      if (request.params.command === 'off') {
+        torch.off();
+	reply('Light OFF');
+      }
+      reply('Form your request properly.');
+    }
+  });
+
+  server.start((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+
+  this.on('exit', function () {
+    torch.off();
+  });
 });
