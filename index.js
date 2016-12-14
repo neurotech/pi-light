0e0d6c4e26564d5cd0fc8a055a4c5737681946e7
Tim Douglas <neurotech@gmail.com>
2016-12-14 21:41:44 +1100

    Added simple route

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: process.env.PI_LIGHT_HTTP_PORT });
 
+server.route({
+  method: 'POST',
+  path: '/light',
+  handler: function (request, reply) {
+    console.log('Light called!');
+    reply('Hello!');
+  }
+});
+
 server.start((err) => {
   if (err) {
     throw err;
