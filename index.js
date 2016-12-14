b1082a5b41ab96a60005b048663bc5bdce445ef4
Tim Douglas <neurotech@gmail.com>
2016-12-15 10:34:56 +1100

    Removed comments

--- index.js
+++ index.js
@@ -1,63 +1,43 @@
-// const Raspi = require('raspi-io');
-// const five = require('johnny-five');
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-// const board = new five.Board({
-//   io: new Raspi(),
-//   repl: false
-// });
+const board = new five.Board({
+  io: new Raspi(),
+  repl: false
+});
 
-// let pin = 'P1-12';
-// let step = 2000;
+let pin = 'P1-12';
+let step = 2000;
 
 server.connection({ port: process.env.PI_LIGHT_HTTP_PORT });
-server.route({
-  method: 'POST',
-  path: '/light/{command}',
-  handler: function (request, reply) {
-    if (request.params.command === 'on') {
-      reply(`Light ${request.params.command.toUpperCase()}`);
-    }
-    if (request.params.command === 'off') {
-      reply(`Light ${request.params.command.toUpperCase()}`);
-    }
-    if (request.params.command !== 'on' && request.params.command !== 'off') {
-      reply('Form your request properly!');
-    }
-  }
-});
 
-server.start((err) => {
-  if (err) { throw err; }
-  console.log(`Server running at: ${server.info.uri}`);
-});
+board.on('ready', function () {
+  let torch = new five.Led(pin);
 
-// board.on('ready', function () {
-//   let torch = new five.Led(pin);
-//
-//   server.route({
-//     method: 'POST',
-//     path: '/light/{command}',
-//     handler: function (request, reply) {
-//       if (request.params.command === 'on') {
-//         torch.pulse(step);
-//         reply(`Light ${request.params.command.toUpperCase()}`);
-//       }
-//       if (request.params.command === 'off') {
-//         torch.stop().off();
-//         reply(`Light ${request.params.command.toUpperCase()}`);
-//       }
-//       if (request.params.command !== 'on' && request.params.command !== 'off') {
-//         reply('Form your request properly!');
-//       }
-//     }
-//   });
-//
-//   server.start((err) => {
-//     if (err) { throw err; }
-//     console.log(`Server running at: ${server.info.uri}`);
-//   });
-//
-//   this.on('exit', function () { torch.off(); });
-// });
+  server.route({
+    method: 'POST',
+    path: '/light/{command}',
+    handler: function (request, reply) {
+      if (request.params.command === 'on') {
+        torch.pulse(step);
+        reply(`Light ${request.params.command.toUpperCase()}`);
+      }
+      if (request.params.command === 'off') {
+        torch.stop().off();
+        reply(`Light ${request.params.command.toUpperCase()}`);
+      }
+      if (request.params.command !== 'on' && request.params.command !== 'off') {
+        reply('Form your request properly!');
+      }
+    }
+  });
+
+  server.start((err) => {
+    if (err) { throw err; }
+    console.log(`Server running at: ${server.info.uri}`);
+  });
+
+  this.on('exit', function () { torch.off(); });
+});
